b959c6b1a3fd734e55789ca75a633445a1a39884
Alimentados <alimentadostemp@gmail.com>
2025-04-17 10:43:33 -0400

    chore: config husky

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { FlatCompat } from '@eslint/eslintrc';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,21 +11,21 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   {
-    ignores: ["src/http/api.ts"],
+    ignores: ['src/http/api.ts'],
   },
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
   {
     // ignorePatterns: ["src/http/api.ts"],
     rules: {
-     'react-hooks/rules-of-hooks': 'off',
+      'react-hooks/rules-of-hooks': 'off',
       '@typescript-eslint/no-empty-object-type': 'off',
       'react-refresh/only-export-components': 'off',
       '@typescript-eslint/no-explicit-any': 'off',
       'no-constant-binary-expression': 'off',
       'react/display-name': 'off',
       '@typescript-eslint/ban-ts-comment': 'off',
-    }
-  }
+    },
+  },
 ];
 
 export default eslintConfig;
--- src/components/navigation/stepper/stepper.tsx
+++ src/components/navigation/stepper/stepper.tsx
@@ -8,17 +8,17 @@ interface StepperProps {
 export function Stepper({ steps, currentStep }: StepperProps) {
   return (
     <div className='flex flex-col items-end justify-center'>
-      <div className='flex gap-3 w-full'>
+      <div className='flex w-full gap-3'>
         {[...Array(steps)].map((_, i) => (
           <div
             key={i}
-            className={clsx('h-2 w-full bg-line rounded-full', {
+            className={clsx('bg-line h-2 w-full rounded-full', {
               '!bg-brand-default': currentStep > i,
             })}
           />
         ))}
       </div>
-      <div className='font-bold text-sm pt-1.5'>
+      <div className='pt-1.5 text-sm font-bold'>
         Passo {currentStep} de {steps}
       </div>
     </div>
